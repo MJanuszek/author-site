2e0f3bcc7432ba32ee98b118d34156481d4c42b6
Monika Januszek <monika.januszek.mail@gmail.com>
2026-01-25 09:41:08 +0100

    announcement premiere 03

--- src/News.jsx
+++ src/News.jsx
@@ -6,6 +6,37 @@ const News = function () {
   return (
     <div className="news">
       <Navigation />
+      {/* ------------------------------------------------------------ */}
+      <div className="announce">
+        <div className="announce-card">
+          <div className="announce-cover announce-cover--premiere"></div>
+
+          <div className="announce-description">
+            <p className="announce-desc">
+              Czy można spotkać kogoś po latach… i odkryć, że nic nie jest
+              takie, jak zapamiętaliśmy?
+            </p>
+
+            <p className="announce-desc">
+              Eliza i Feliks w czasach szkoły średniej byli parą. Jednak z
+              jakiegoś, niejasnego dla ich przyjaciół powodu, rozstali się tuż
+              przed maturą. I nigdy już nie zamienili z sobą ani słowa. Los
+              jednak bywa przewrotny…
+            </p>
+
+            <p className="announce-desc">
+              Gdy widzą się pierwszy raz po latach, dawne wspomnienia odżywają.
+              Czy znana i ceniona aktorka nadal ma coś wspólnego z nieśmiałą
+              uczennicą? Czy ceniony specjalista od luksusowych aut, nadal ma
+              duszę nicponia, którym był w czasach szkoły średniej? Jakie
+              nieporozumienie położyło się cieniem na życiu tych dwojga i
+              sprawiło, że się rozstali? Czy uczucie z lat szkolnych odżyje?
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* ---------------------------------------------------------- */}
       <div className="event-new-book">
         <h1>Nowości 2026!</h1>
         {/* <div className="new-book-cover"></div> */}
